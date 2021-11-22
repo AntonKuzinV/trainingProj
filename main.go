7e61eaa5ca930809b90d487a2504cc5498d3f432
kuzin-a <antonkuzin1999@gmail.com>
2021-11-22 18:08:43 +0200

    Fixed app according to task requirements

--- main.go
+++ main.go
@@ -1,72 +1,14 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/segmentio/kafka-go"
 	"golang.org/x/net/websocket"
 	"log"
 	"net/http"
-	"time"
 	"trainingProj/websockets"
 )
 
 func main() {
-	topic := "quickstart-events"
-	partition := 0
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
-	if err != nil {
-		log.Fatal("failed to dial leader: ", err)
-	}
-
-	w := &kafka.Writer{
-		Addr:     kafka.TCP("localhost:9092"),
-		Topic:    topic,
-		Balancer: &kafka.LeastBytes{},
-	}
-	//writing some messages
-	err = w.WriteMessages(context.Background(),
-		kafka.Message{
-			Key:   []byte("Key-A"),
-			Value: []byte("Hello World!"),
-		},
-		kafka.Message{
-			Key:   []byte("Key-B"),
-			Value: []byte("One!"),
-		},
-		kafka.Message{
-			Key:   []byte("Key-C"),
-			Value: []byte("Two!"),
-		},
-	)
-	if err != nil {
-		log.Fatal("failed to write messages:", err)
-	}
-
-	if err := w.Close(); err != nil {
-		log.Fatal("failed to close writer:", err)
-	}
-
-	err = conn.SetReadDeadline(time.Now().Add(1 * time.Second))
-	if err != nil {
-		return
-	}
-	//read some messages
-	batch := conn.ReadBatch(10e3, 1e6)
-
-	b := make([]byte, 10e3)
-	for {
-		n, err := batch.Read(b)
-		if err != nil {
-			break
-		}
-		fmt.Println(string(b[:n]))
-	}
-	if err = conn.Close(); err != nil {
-		log.Fatal("failed to close connection: ", err)
-	}
 	http.Handle("/", websocket.Handler(websockets.Echo))
-
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
--- websockets/server.go
+++ websockets/server.go
@@ -6,24 +6,21 @@ import (
 )
 
 func Echo(ws *websocket.Conn) {
-	var err error
-
 	for {
+		var err error
+		msg := `Message from backend`
+		for i := 0; i < 10; i++ {
+			if err = websocket.Message.Send(ws, msg); err != nil {
+				fmt.Println("Can't send")
+			} else {
+				fmt.Println("Sending")
+			}
+		}
 		var reply string
-
 		if err = websocket.Message.Receive(ws, &reply); err != nil {
 			fmt.Println("Can't receive")
 			break
 		}
-
-		fmt.Println("Received back from client: " + reply)
-
-		msg := "Received:  " + reply
-		fmt.Println("Sending to client: " + msg)
-
-		if err = websocket.Message.Send(ws, msg); err != nil {
-			fmt.Println("Can't send")
-			break
-		}
+		fmt.Println(reply)
 	}
 }
